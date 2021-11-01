dc08a7746fbfa99f1b9b5b4c0e84b4874d20bfba
s207017 <choish03030@gmail.com>
2021-11-01 14:01:32 +0800

    updated reservation

--- Project/src/Restaurant/Reservation.java
+++ Project/src/Restaurant/Reservation.java
@@ -19,43 +19,15 @@ public class Reservation {
         this.contactNumber = contactNumber;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
+    //The following overloaded methods for changes in reservation details
+    //#######################################################################
+    public void changeReservation(Date date, LocalTime time){
         this.date = date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
         this.time = time;
     }
 
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public int getPax() {
-        return pax;
-    }
-
-    public void setPax(int pax) {
-        this.pax = pax;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
+    public void changeReservation(String contactNumber){
         this.contactNumber = contactNumber;
     }
+
 }
