db1799eb679a83a60994bc008b7f3ab7d6883bb7
deionna <64540745+deionna@users.noreply.github.com>
2021-11-02 20:42:49 +0800

    accidentally added comments :')

--- Project/src/Restaurant/Table.java
+++ Project/src/Restaurant/Table.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package Restaurant;
 
 import java.util.ArrayList;
@@ -67,71 +66,3 @@ public class Table {
         this.reservations.add(reservation);
     }
 }
-=======
-//package Restaurant;
-//
-//import java.util.ArrayList;
-//
-//public class Table {
-//    private int tableNum;
-//    private int tableCap;
-//    private int pax;
-//    private Order order;
-//    enum Level {
-//        FREE,
-//        RESERVED,
-//        OCCUPIED
-//    }
-//    private Level tableStatus;
-//    private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
-//    public Table(int tableNum, int tableCap, int pax){
-//        this.tableNum = tableNum;
-//        this.tableCap = tableCap;
-//        this.pax = pax;
-//        this.tableStatus = Level.FREE;
-//    }
-//    public int getTableNum(){
-//        return tableNum;
-//    }
-//
-//    public int getTableCap(){
-//        return tableCap;
-//    }
-//
-//    public Level getTableStatus(){
-//        return tableStatus;
-//    }
-//
-//    public void reserveTable(int pax){
-//        if(pax<=tableCap){
-//            this.pax = pax;
-//            this.tableStatus = Level.RESERVED;
-//        }
-//    }
-//
-//    public void occupyTable(int pax){
-//        if(pax<=tableCap){
-//            this.pax = pax;
-//            this.tableStatus = Level.OCCUPIED;
-//        }
-//    }
-//    public void freeTable(){
-//        this.pax = 0;
-//        this.tableStatus = Level.FREE;
-//    }
-//    public void assignOrder(Order order){
-//        this.order = order;
-//    }
-//    public Order getOrder(){
-//        return this.order;
-//    }
-//
-//    public ArrayList<Reservation> getReservations() {
-//        return reservations;
-//    }
-//
-//    public void addReservation(Reservation reservation){
-//        this.reservations.add(reservation);
-//    }
-//}
->>>>>>> Stashed changes
