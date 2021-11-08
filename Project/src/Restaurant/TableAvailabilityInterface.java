8aa86f2d29d9cc4f7170de6efba97354d6eb0c2d
AnanyaB1 <63778650+AnanyaB1@users.noreply.github.com>
2021-11-08 22:20:56 +0800

    jkd

--- Project/src/Restaurant/TableAvailabilityInterface.java
+++ Project/src/Restaurant/TableAvailabilityInterface.java
@@ -2,9 +2,14 @@ package Restaurant;
 
 public class TableAvailabilityInterface {
     private Restaurant r;
-    public TableAvailabilityInterface(Restaurant r, Table t){
+    public TableAvailabilityInterface(Restaurant r){
         this.r = r;
     }
+
+    public void printTableAvailability(){
+        System.out.print(r.toString());
+    }
+
 
 
 }
--- Project/src/Restaurant/TestTableAvailabilityInterface.java
+++
@@ -1,9 +0,0 @@
-package Restaurant;
-
-public interface TestTableAvailabilityInterface {
-    public static void main(String[] arg) {
-        Menu menu = new Menu();
-        Restaurant r = new Restaurant();
-        TableAvailabilityInterface tableAvailabilityInterface= new TableAvailabilityInterface(r);
-    }
-}
