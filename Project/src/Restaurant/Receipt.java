049852df83940f46cd5de14fe6436c9c7056440e
AnanyaB1 <63778650+AnanyaB1@users.noreply.github.com>
2021-11-09 10:23:00 +0800

    idk what this is!

--- Project/src/Restaurant/Receipt.java
+++ Project/src/Restaurant/Receipt.java
@@ -32,7 +32,7 @@ public class Receipt {
         System.out.print("Order ID: ");
         for (Table table: this.payment.getTables()) {
             orderIdList += table.getOrder().getOrderID() + " ";
-            tableList += table.getId() + " ";
+            tableList += table.getTableNum() + " ";
         }
         System.out.println("Order ID: " + tableList);
         System.out.println("Order ID: " + orderIdList);
