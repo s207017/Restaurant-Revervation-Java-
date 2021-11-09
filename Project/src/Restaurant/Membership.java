99b970b773be100acef3fc5cd0b2008eef4af949
deionna <64540745+deionna@users.noreply.github.com>
2021-11-09 14:44:40 +0800

    done w payment interface

--- Project/src/Restaurant/Membership.java
+++ Project/src/Restaurant/Membership.java
@@ -6,23 +6,23 @@ public class Membership {
     ArrayList<Member> membersList = new ArrayList<Member>();
 
     public Membership(){
-        membersList.add(new Member("98357328"));
-        membersList.add(new Member("84457564"));
-        membersList.add(new Member("92925963"));
-        membersList.add(new Member("92715388"));
-        membersList.add(new Member("82943623"));
-        membersList.add(new Member("87569116"));
-        membersList.add(new Member("95431183"));
-        membersList.add(new Member("96789030"));
-        membersList.add(new Member("97420100"));
-        membersList.add(new Member("98517739"));
+        membersList.add(new Member(98357328));
+        membersList.add(new Member(84457564));
+        membersList.add(new Member(92925963));
+        membersList.add(new Member(92715388));
+        membersList.add(new Member(82943623));
+        membersList.add(new Member(87569116));
+        membersList.add(new Member(95431183));
+        membersList.add(new Member(96789030));
+        membersList.add(new Member(97420100));
+        membersList.add(new Member(98517739));
     }
 
     public void addMember(Member member){
         membersList.add(member);
     }
 
-    public boolean checkMembership(String number){
+    public boolean checkMembership(int number){
         for (Member member: membersList){
             if (member.getNumber()== number)
                 return true;
