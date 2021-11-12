0bf40330aba08fe8f63ed539e0ee38ce41f1c999
oshwad <lkl.faith05@gmail.com>
2021-11-13 00:05:16 +0800

    change salerevperiod to a subclass of salerevday

--- Project/src/Restaurant/SalesRevenueReportController.java
+++ Project/src/Restaurant/SalesRevenueReportController.java
@@ -7,23 +7,6 @@ public class SalesRevenueReportController {
     }
 
     /**
-     * creates a new sales report for a specified period
-     * choice == 1 in SaleRevenueReport constructor will prompt user to enter 2 dates
-     * to get the period for which they wish to view the report
-     */
-    public void createPeriodSalesReport(){
-        SalesRevenueReport salesReport = new SalesRevenueReport(restaurant.getTransactionHistory(), 1);
-    }
-
-    /**
-     * creates a new sales report for a specified day
-     * choice == 2 in SaleRevenueReport constructor prompts user to enter one date
-     */
-    public void createDaySalesReport(){
-        SalesRevenueReport salesReport = new SalesRevenueReport(restaurant.getTransactionHistory(), 2);
-    }
-
-    /**
      * method to be called in the main app for generating sales revenue reports
      */
     public void printSalesRevenueReport(){
@@ -36,20 +19,19 @@ public class SalesRevenueReportController {
         System.out.println("(1) Period report");
         System.out.println("(2) Day report");
         System.out.println("(3) Return to main app");
-        do{
-            System.out.print("Enter option: ");
-            choice = GetInput.getIntFromRange(1,3);
-        }while(choice != 1 && choice != 2 && choice != -1);
+        System.out.print("Enter option: ");
+        choice = GetInput.getIntFromRange(1,3);
         switch(choice){
             case 1:
-                createPeriodSalesReport();
+                SalesRevenuePeriod sp = new SalesRevenuePeriod();
+                sp.createReport(restaurant.getTransactionHistory());
                 break;
             case 2:
-                createDaySalesReport();
+                SalesRevenueDay sd = new SalesRevenueDay();
+                sd.createReport(restaurant.getTransactionHistory());
                 break;
-            case -1:
+            case 3:
                 break;
         }
-        return;
     }
 }
