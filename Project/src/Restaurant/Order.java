f58ff181605b60df1b54cffed669c43cd0be22d6
ihsunday <92501736+ihsunday@users.noreply.github.com>
2021-11-14 03:05:19 +0800

    Javadoc
    Changed order and salesrevenueday

--- Project/src/Restaurant/Order.java
+++ Project/src/Restaurant/Order.java
@@ -5,17 +5,55 @@ import java.time.LocalDateTime;
 import java.sql.Timestamp;
 import java.util.Scanner;
 
+/**
+ * An instance of this object represents one order
+ */
 public class Order {
+    /**
+     * Static variable to represent the order number of this order
+     * To be used by OrderID
+     */
     private static int orderNum = 0;
+    /**
+     * Order ID of this number
+     */
     private int orderID;
+    /**
+     * Staff ID of the staff in charge of taking this order
+     */
     private int staffID;
+    /**
+     * Table number that this order is assigned to
+     */
     private int tableNum;
+    /**
+     * Date that this order is made
+     */
     private LocalDateTime date;
+    /**
+     * Total price of this order
+     */
     private double total;
+    /**
+     * Reference to ArrayList of order items
+     */
     private ArrayList<OrderItem> orderList;
+    /**
+     * Static menu declared to be used in mnethos of this Order
+     */
     private static Menu menu;
 
+    /**
+     * Increments orderNum to be assigned to orderID
+     */
     {orderNum +=1;}
+
+    /**
+     * Constructor takes in staff ID, the table number, and the menu to create an Order
+     * @param staffID Staff ID of staff in charge of taking order
+     * @param tableNum Table number that this order is assigned to
+     * @param menu Menu for reference for this Order
+     */
     public Order(int staffID, int tableNum,Menu menu) {
         this.orderID = orderNum;
         this.staffID = staffID;
@@ -24,12 +62,65 @@ public class Order {
         this.orderList = new ArrayList<>();
         this.menu = menu;
     }
+
+    /**
+     * Gets Order ID of this order
+     * @return Returns order ID of this order as an integer
+     */
+    public int getOrderID(){
+        return this.orderID;
+    }
+
+    /**
+     * Gets Staff ID of staff in charge of this order
+     * @return Returns order ID of staff in charge of this order
+     */
+    public int getStaffID(){
+        return this.staffID;
+    }
+
+    /**
+     * Gets Table number that this order is assigned to
+     * @return Returns table number that this order is assigned to as an integer
+     */
+    public int getTableNum(){
+        return this.tableNum;
+    }
+
+    /**
+     * Gets date of this order
+     * @return Returns date of this order as a LocalDateTime object
+     */
+    public LocalDateTime getDate(){
+        return date;
+    }
+
+    /**
+     * Gets list of order items in this order
+     * @return Returns list of order items in this order as an ArrayList
+     */
+    public ArrayList<OrderItem> getOrderItemList(){
+        return this.orderList;
+    }
+
+    /**
+     * Gets total price of this order
+     * @return Returns price of this order as a double
+     */
     public double getTotal(){
         return this.total;
     }
 
     /**
-     *
+     * Adds to total price of this order
+     * @param amount Price to be added to this order as a double
+     */
+    public void addTotal(double amount){
+        this.total += amount;
+    }
+
+    /**
+     * Checks this order's order item list for the existence of a specific item
      * @param itemID ID of the menuItem to be checked
      * @return Returns the index of the requested item in the orderItem array
      */
@@ -44,9 +135,9 @@ public class Order {
     }
 
     /**
-     *
-      * @param item Item to be added to the order
-     * @param quantity Quantity of item to be added to the order
+     * Adds items to this order
+      * @param item Item to be added to this order
+     * @param quantity Quantity of item to be added to this order
      */
     public void addOrderItems(MenuItem item, int quantity){
         int index = checkItemExistence(item.getItemID());
@@ -59,13 +150,18 @@ public class Order {
     }
 
     /**
-     *
+     * Removes items from order
      * @param index Index of menuItem in the orders orderItem arraylist
      * @param quantity Quantity of item to be deleted
      */
     public void removeOrderItems(int index, int quantity){
         this.orderList.get(index).subtractQuantityOrdered(quantity);
     }
+
+    /**
+     * Checks if order is completed
+     * @return Boolean that indicates if order is completed
+     */
     public boolean isOrderCompleted(){
         for(int i = 0;i < orderList.size();i++){
             if(orderList.get(i).getQuantityCompleted() < orderList.get(i).getQuantityOrdered()){
@@ -74,25 +170,10 @@ public class Order {
         }
         return true;
     }
-    public int getOrderID(){
-        return this.orderID;
-    }
-    public int getStaffID(){
-        return this.staffID;
-    }
-    public int getTableNum(){
-        return this.tableNum;
-    }
-    public LocalDateTime getDate(){
-        return date;
-    }
-    public ArrayList<OrderItem> getOrderItemList(){
-        return this.orderList;
-    }
-    public void addTotal(double amount){
-        this.total += amount;
-    }
 
+    /**
+     * Prints order by iterating through its order list getting each item's name and quantity ordered
+     */
     public void printOrder(){
         System.out.printf("Order: %d for Table Number: %d\n",this.orderID,this.tableNum);
         System.out.printf("%4s%2s %-38s| %-4s\n","ID","|","ITEM NAME","QTY");
