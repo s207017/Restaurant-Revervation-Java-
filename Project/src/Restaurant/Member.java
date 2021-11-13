f25a8c006cb01ffdd40a1120fdd9b273ffac92b8
deionna <64540745+deionna@users.noreply.github.com>
2021-11-14 01:49:23 +0800

    member and memberships

--- Project/src/Restaurant/Member.java
+++ Project/src/Restaurant/Member.java
@@ -1,12 +1,23 @@
 package Restaurant;
 
 public class Member {
+    /**
+     * The phone number of this member.
+     */
     private int Number;
 
+    /**
+     * The constructor for member.
+     * @param number The phone number of this member.
+     */
     public  Member(int number) {
         Number = number;
     }
 
+    /**
+     * Getter for phone number of this member.
+     * @return The phone number of this member.
+     */
     public int getNumber() {
         return Number;
     }
--- Project/src/Restaurant/Membership.java
+++ Project/src/Restaurant/Membership.java
@@ -3,14 +3,24 @@ package Restaurant;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ *An instance of this class represents a single membership.
+ */
 public class Membership {
+    /**
+     * The list of members of the restaurant.
+     */
     ArrayList<Member> membersList = new ArrayList<Member>();
 
+    /**
+     * The constructor for membership.
+     * Reads the phone number of every member from the members.txt file and creates new members which are added to the list of members in this membership.
+     * @throws IOException
+     */
     public Membership() throws IOException {
         BufferedReader membersText = new BufferedReader(
                 new FileReader("./textfiles/members.txt")
         );
-
         // for reading items from text file
         int phoneNum = 0;
         String s;
@@ -22,14 +32,28 @@ public class Membership {
         membersText.close();
     }
 
+    /**
+     * Getter for membersList of this membership.
+     * @return
+     */
     public ArrayList<Member> getMembersList() {
         return membersList;
     }
 
+    /**
+     * Adds member to the list of members in this membership.
+     * @param member
+     * @throws IOException
+     */
     public void addMember(Member member) throws IOException {
         membersList.add(member);
     }
 
+    /**
+     * Checks if a person is a member in the list of members in this membership using phone number.
+     * @param number The phone number of a person.
+     * @return The index of the member in the list of members if the member exists. Else, 0.
+     */
     public int checkMembership(int number){
         int count = 0;
         for (Member member: membersList){
@@ -40,6 +64,10 @@ public class Membership {
         return 0;
     }
 
+    /**
+     * Updates the members.txt file by overwriting the text file. Writes the phone number of every member in the list of members in this membership.
+     * @throws IOException
+     */
     public void updateMembershipText() throws IOException {
         BufferedWriter bw = new BufferedWriter(
                 new FileWriter("./textfiles/members.txt", false)
